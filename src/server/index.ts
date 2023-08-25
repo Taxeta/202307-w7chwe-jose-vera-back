cd7dde97f161de349771d67dc3377c13661b4f52
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-25 16:16:41 +0200

    Create robotsRouters and pingController router

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import { pingController } from "./controllers/pingController.js";
+import robotsRouter from "./routers/robotsRouters.js";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/", pingController);
+
+app.use("/robots", robotsRouter);
 
 export default app;
