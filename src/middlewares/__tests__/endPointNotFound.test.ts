18abfdfb6f668bd8279eb401ec16e58d25114593
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-08-25 18:24:54 +0200

    Test endpoint custom error

---
+++ src/middlewares/__tests__/endPointNotFound.test.ts
@@ -0,0 +1,22 @@
+import { type NextFunction, type Request, type Response } from "express";
+import CustomError from "../CustomError";
+import { endpointNotFound } from "../errorHandlers";
+
+describe("Given an endpointNotFound middleware", () => {
+  describe("When it receives an error", () => {
+    test("Then it should calla next function and return an error like 'Endpoint not found'", () => {
+      const req: Partial<Request> = {};
+      const res: Partial<Response> = {};
+      const next: NextFunction = jest.fn();
+      const expectedCustomError = new CustomError(
+        "Error, robots not found",
+        404,
+        "Error, robots not found",
+      );
+
+      endpointNotFound(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(expectedCustomError);
+    });
+  });
+});
--- src/middlewares/errorHandlers.ts
+++ src/middlewares/errorHandlers.ts
@@ -1,5 +1,19 @@
 import { type NextFunction, type Request, type Response } from "express";
-import type CustomError from "./CustomError";
+import CustomError from "./CustomError.js";
+
+export const endpointNotFound = (
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const newCustomError = new CustomError(
+    "Error, robots not found",
+    404,
+    "error, robots not found",
+  );
+
+  next(newCustomError);
+};
 
 export const generalErrorHandler = (
   error: CustomError,
@@ -7,18 +21,8 @@ export const generalErrorHandler = (
   res: Response,
   _next: NextFunction,
 ) => {
-  const errorMessage = error.message ?? "Error";
+  const errorMessage = error.message ?? "Error, robot not found";
   const errorStatusCode = error.statusCode ?? 500;
 
   res.status(errorStatusCode).json({ error: errorMessage });
-};
-
-export const endpointNotFound = (
-  _req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const errorNotFound = res.status(404).json({ error: "Endpoint not found" });
-
-  next(errorNotFound);
 };
